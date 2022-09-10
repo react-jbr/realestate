9d252785036d399d201edea7d426929fc1e42ca2
jabeermk <jabeermk@gmail.com>
2022-09-10 13:34:46 +0530

    fotter cahnges1

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,8 @@ import { Box } from '@chakra-ui/layout';
 
 const Footer = () => (
   <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
-    © 2022 DLF, Inc.
-  </Box>
+    © 2021-2022 DLF, Inc.
+   </Box>
 );
 
 export default Footer;
