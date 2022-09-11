24e0f48d3367ea9ef1f7ec23a07290f7dc40e6a1
jabeermk <jabeermk@gmail.com>
2022-09-11 21:49:22 +0530

    iteration changed on search filter

--- components/ImageScrollbar.js
+++ components/ImageScrollbar.js
@@ -45,6 +45,7 @@ export default function ImageSrollbar({ data }) {
       {data.map((item) => (
         <Box width="910px" itemId={item.id} overflow="hidden" p="1">
           <Image
+          key={item.id}
             placeholder="blur"
             blurDataURL={item.url}
             src={item.url}
