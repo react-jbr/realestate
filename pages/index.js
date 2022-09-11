d23cdd13f530762d2b807089f96d63f167b44cc0
jabeermk <jabeermk@gmail.com>
2022-09-11 07:16:01 +0530

    filter data

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 import { baseUrl,fetchApi } from '../utils/fetchApi';
 import Property from '../components/Property';
+import Search from './search';
 
 
 export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
